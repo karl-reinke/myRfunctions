6457b5b3802e3a1a4550baffb55786f8b79dd472
karl-reinke <126909048+karl-reinke@users.noreply.github.com>
2023-08-19 11:03:11 -0500

    Update pseudoRsquare.R

--- pseudoRsquare.R
+++ pseudoRsquare.R
@@ -9,12 +9,15 @@ pseudoRsquare <- function(model.1, model.2, component = "residual"){
       print("============PseudoR2 a la Shiverdecker & LeBreton (2019)============")
       print("============== Formula is: 'error1 - error2 / error1' ==============")
       print("=== Proportion of error variance in Model 1 explained by Model 2 ===")
-      print(round((error1 - error2) / error1, 10))
+      print(paste0("pseudo R2: ",round((error1 - error2) / error1, 10)))
     }
     if(component == "(Intercept)"){
       model.1 = VarCorr(model.1); model.2 = VarCorr(model.2)
       var1 = as.numeric(model.1[1,"Variance"])
       var2 = as.numeric(model.2[1,"Variance"])
+      print("==============PseudoR2 a la Shiverdecker & LeBreton (2019)==============")
+      print("=================== Formula is: 'Int1 - Int2 / Int1' ===================")
+      print("=== Proportion of Intercept variance in Model 1 explained by Model 2 ===")
       print(round((var1 - var2) / var1, 10))
     }
   }
@@ -23,6 +26,9 @@ pseudoRsquare <- function(model.1, model.2, component = "residual"){
     if(component == "residual"){
       error1 = attr(VarCorr(model.1), "sc")^2
       error2 = attr(VarCorr(model.2), "sc")^2
+      print("============PseudoR2 a la Shiverdecker & LeBreton (2019)============")
+      print("============== Formula is: 'error1 - error2 / error1' ==============")
+      print("=== Proportion of error variance in Model 1 explained by Model 2 ===")
       print(round((error1 - error2) / error1, 10))
     }
     if(!component == "residual"){
